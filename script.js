debb52ffe67e0210db6161785ef8190a52938ff7
Sam K <sakw975257@gmail.com>
2022-08-24 23:01:22 -0500

    Add time

--- script.js
+++ script.js
@@ -3,6 +3,39 @@ let year = d.getFullYear();
 let month = d.getMonth() + 1; 
 let day = d.getDate();
 
+let d1 = new Date();
+let hours1 = d1.getHours();
+let minutes1 = d1.getMinutes();
+
+if (hours1 > 12) {
+    hours1 = hours1 - 12;
+}
+
+if (minutes1 < 10) {
+    minutes1 = "0" + minutes1
+}
+
+document.querySelector('.clock').innerHTML = `${hours1}:${minutes1}`;
+
+function getTime() {
+    let d2 = new Date();
+    let hours2 = d2.getHours();
+    let minutes2 = d2.getMinutes();
+
+    if (hours2 > 12) {
+        hours2 = hours2 - 12;
+    }
+
+    if (minutes2 < 10) {
+        minutes2 = "0" + minutes2
+    }
+
+    document.querySelector('.clock').innerHTML = `${hours2}:${minutes2}`;
+    console.log(`${hours2}:${minutes2}`);
+};
+
+setInterval(getTime, 1000);
+
 function getImage() {
     fetch(`https://api.nasa.gov/planetary/apod?date=${year + "-" + month + "-" + day}&api_key=${config.apiKey}`)
         .then((response) => response.json())
